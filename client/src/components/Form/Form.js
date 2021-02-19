2c7b2a9086f4b66917f03be89d2a618e0767cd2e
Isabella <isabellasilvagreco@gmail.com>
2021-02-19 14:02:00 +0000

    update function

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import useStyles from './styles';
 import { textField, Button, Typography, Paper, TextField } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
-import { createPost } from '../../actions/posts';
+import { useDispatch, useSelector } from 'react-redux';
+import { createPost, updatePost } from '../../actions/posts';
 
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
         creator: " ",
         title: " ",
@@ -14,24 +14,43 @@ const Form = () => {
         tags: " ",
         selectedFile: " "
     })
-
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        if (post) setPostData(post);
+    }, [post])
+
     const handleSubmit = (e) => {
+
         e.preventDefault();
-        dispatch(createPost(postData));
+
+        if (currentId) {
+            dispatch(updatePost(currentId, postData));
+        } else {
+            dispatch(createPost(postData));
+
+        }
+        clear();
     }
 
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({
+            creator: " ",
+            title: " ",
+            message: " ",
+            tags: " ",
+            selectedFile: " "
+        })
     }
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.form} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography className={classes.title} variant="h6">
-                    Creating a new post
+                    {!currentId ? 'Creating a new' : 'Editing a'} post
             </Typography>
                 <TextField
                     className={classes.input}
